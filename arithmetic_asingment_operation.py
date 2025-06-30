d7697b3505bcb5e6a7ac79de41d8f073da5c9a91
navanithaadhav <navanithaweb@gmail.com>
2025-06-30 23:52:18 +0530

    feat: operator

---
+++ arithmetic_asingment_operation.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 31 12:15:41 2022
+
+@author: ADMIN
+"""
+
+#Arithmetic assingment operation
+a=125
+a+=5
+print(a)
+a-=10
+print(a)
+a*=10
+print(a)
+a/=10
+print(a)
+a%=10
+print(a)
+a//=10
+print(a)
+a**=10
+print(a)
