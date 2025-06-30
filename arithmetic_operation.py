2df289457df5ae0d238736bde952178c116baf07
navanithaadhav <navanithaweb@gmail.com>
2025-06-30 23:53:54 +0530

    feat: operator

---
+++ arithmetic_operation.py
@@ -0,0 +1,20 @@
+#Arithmatic operation
+"""
++ ADDITION
+- SUNTRATION
+* MULTIPLICATION
+/ DIVISION
+% MODULOS
+** EXPONENTIATION
+// FLOOR DIVISION
+"""
+#ADDITION
+a=124
+b=10
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(2**3)
